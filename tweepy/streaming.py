66c3265dbcae3de3c3bf3c4af5882e6a92456a41
Fansen <sen.fan@tradingfront.cn>
2017-08-21 10:01:21 +0800

    stream disconnect

--- tweepy/streaming.py
+++ tweepy/streaming.py
@@ -469,3 +469,22 @@ class Stream(object):
         if self.running is False:
             return
         self.running = False
+
+        import threading
+        import time
+        import inspect
+        import ctypes
+
+        tid = self._thread.ident
+        tid = ctypes.c_long(tid)
+        exctype = SystemExit
+        if not inspect.isclass(exctype):
+            exctype = type(exctype)
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+        if res == 0:
+            raise ValueError("invalid thread id")
+        elif res != 1:
+            # """if it returns a number greater than one, you're in trouble,
+            # and you should call it again with exc=NULL to revert the effect"""
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
+            raise SystemError("PyThreadState_SetAsyncExc failed")
